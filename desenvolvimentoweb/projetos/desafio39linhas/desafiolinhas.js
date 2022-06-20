b21594e96e85368548f9820b3ac4d77fab49f247
DarleyLopes21 <darleylopes7@gmail.com>
2022-06-19 23:57:32 -0300

    iclusao arquivos arraypares e desafiolinhas

---
+++ desenvolvimentoweb/projetos/desafio39linhas/desafiolinhas.js
@@ -0,0 +1,10 @@
+function desafiolinhas(){
+    let horilinha = ('---------------------------------------');
+    let verlinha = ('|                                     |');
+    console.log(horilinha);
+    for(i=0;i<5;i++){
+        console.log(verlinha);   
+    }
+    console.log(horilinha);
+}
+desafiolinhas()
